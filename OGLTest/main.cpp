3be27ed7e8bad753e83a959a04a7ead26bf44d49
Emiel Bon <e.bon@itude.com>
2014-01-29 13:58:55 +0100

    Initial Commit

---
+++ OGLTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OGLTest
+//
+//  Created by Emiel Bon on 29-01-14.
+//  Copyright (c) 2014 Emiel Bon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
